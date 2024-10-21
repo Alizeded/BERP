b094b23daa08f6218844a7a2450ab847ae9f3c2d
Alizeded <lijun.wang@jaist.ac.jp>
2024-10-22 00:51:02 +0900

    updated README.md

---
+++ weights/occupancy_module/occupancy_module_Spectrogram.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# URL of the checkpoint file (BERP occupancy module - MFCC)
+url="https://jstorage.box.com/v/BERP-occupancy-module-spectro"
+
+# open the browser to download the file
+xdg-open $url
+
